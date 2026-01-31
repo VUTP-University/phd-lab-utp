14b83e710c745b71cdbb7b8f1cd3c1bf5dcac9c8
Aleksandar Karastoyanov <a.karastoyanov@utp.bg>
2026-01-31 13:40:34 +0200

    Fixed navbar buttons
    Signed-off-by: Aleksandar Karastoyanov <a.karastoyanov@utp.bg>

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,25 +25,25 @@ export default function NavbarTailwind({ user, onLogout }) {
           </div>
 
           {/* Desktop Menu */}
-          <div className="hidden md:flex items-center gap-4">
-            <a href="/" className="custom_button px-4 py-2">
-              Home
+          <div className="hidden md:flex items-center gap-1">
+            <a href="/" className="px-2 py-2">
+              {t("navbar.home")}
             </a>
 
             {user && (
-              <a href="/dashboard" className="custom_button px-4 py-2">
-                My Courses
+              <a href="/dashboard" className="px-4 py-2">
+                {t("navbar.my_courses")}
               </a>
             )}
 
-            {user && user.is_lab_admin && (
-              <a href="/create-news" className="custom_button px-4 py-2">
+            {/* {user && user.is_lab_admin && (
+              <a href="/create-news" className="px-4 py-2">
                 Създай новина
               </a>
-            )}
+            )} */}
 
             {user && (
-              <button onClick={onLogout} className="custom_button px-4 py-2">
+              <button onClick={onLogout} className="px-4 py-2">
                 Logout
               </button>
             )}
